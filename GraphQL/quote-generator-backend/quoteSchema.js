f973d6b7ce4e382036218ae858509f3028f5807c
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-25 17:10:07 -0400

    schema, fields defined TODO: - fix randomQuote field aggregator - hide confidential info in process.env - publish this api

---
+++ GraphQL/quote-generator-backend/quoteSchema.js
@@ -0,0 +1,42 @@
+const {GraphQLList, GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt} = require("graphql");
+
+const QuoteType = new GraphQLObjectType({
+    name: 'Quote',
+    fields: {
+        id: {
+            type: GraphQLString,
+            resolve: q => q._id
+        },
+        text: {
+            type: GraphQLString,
+            resolve: t => t.quoteText
+        },
+        author: {
+            type: GraphQLString,
+            resolve: t => t.quoteAuthor
+        }
+    }
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {
+        allQuotes: {
+            type: new GraphQLList(QuoteType),
+            description: 'A list of all available quotes',
+            resolve: (_, args, {db}) => db.collection('quote-collection').find().toArray()
+        },
+        count: {
+            type: GraphQLInt,
+            description: 'Count of all stored quotes',
+            resolve: (_, args, {db}) => db.collection('quote-collection').count()
+        },
+        randomQuote: {
+            type: QuoteType,
+            description: 'Returns a randomly picked quote',
+            resolve: (_, args, {db}) => db.collection('quote-collection').aggregate({ $sample: { size: 1 } })
+        }
+    }
+});
+const schema = new GraphQLSchema({query:queryType });
+module.exports = schema;
