a7f97c52ec672e574142c78970f28a7b1b9304bc
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-25 18:13:53 -0400

    adjustments to react components

--- GraphQL/relay-driven-app/src/index.js
+++ GraphQL/relay-driven-app/src/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './app';
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+const mySchema = require('./graphQLSchema');
+const MONGO_URL = "mongodb+srv://fosteman:120308032000@antilope-mjt4x.mongodb.net/test?retryWrites=true&w=majority";
+
+MongoClient.connect(MONGO_URL, (err, db) => {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server');
+
+    app.use('/graphql', graphqlHTTP({
+        schema: schema,
+        context: { db },
+        graphiql: true
+    }));
+
+    app.listen(3000, () =>
+        console.log('Running Express.js on port 3000')
+    );
+});
