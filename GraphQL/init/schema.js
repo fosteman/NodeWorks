7d945d642e95cb5eb0b9ca021a65c01ea233c515
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-24 21:42:06 -0400

    rolling dice

--- GraphQL/init/schema.js
+++ GraphQL/init/schema.js
@@ -1,11 +1,20 @@
 const {
+    GraphQLInt,
+    GraphQLList,
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString
 } = require('graphql');
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
+
 const queryType = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
+        diceRoll: {
+            type: GraphQLList(GraphQLInt),
+            resolve: () => [roll(), roll()]
+        },
         imgURL: {
             type: GraphQLString,
             resolve: () => 'http://imgUrl...'
